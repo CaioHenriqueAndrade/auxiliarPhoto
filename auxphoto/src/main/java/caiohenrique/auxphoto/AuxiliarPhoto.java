ec3698f186b89ea5abbd303b3e134191e25b2a5c
Caio Henrique Andrade Lima <mac@MacBook-Pro-de-Macbook-2.local>
2018-10-01 12:39:16 -0300

    turning better the code

--- auxphoto/src/main/java/caiohenrique/auxphoto/AuxiliarPhoto.java
+++ auxphoto/src/main/java/caiohenrique/auxphoto/AuxiliarPhoto.java
@@ -7,6 +7,8 @@ import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.net.ConnectivityManager;
 import android.os.AsyncTask;
+import android.support.annotation.Nullable;
+import android.support.annotation.WorkerThread;
 import android.util.Log;
 
 import java.io.File;
@@ -26,39 +28,72 @@ public class AuxiliarPhoto {
     private String PATH = "";
     private OnDownloadedListener onDownListener;
 
+    /**
+     * the listener of download of photos
+     *
+     */
     public interface OnDownloadedListener {
 
+        /**
+         * called when the download was completed
+         *
+         * @param bitmap
+         * @param nameImage
+         */
         void onDownloadCompleted(Bitmap bitmap, String nameImage);
 
+        /**
+         * called when the download was completed but
+         * yet be in thread work
+         * @param bt
+         * @param nameImage
+         * @return Bitmap - you can make something
+         * with the bitmap and return
+         */
+        @WorkerThread
         Bitmap onDownloadCompletedInBackGround(Bitmap bt, String nameImage);
     }
 
+    /**
+     * set the listener of download
+     * call after download the photo,
+     * first time it call in Worker Thread
+     * and after it call in Main Thread
+     *
+     * @param onDownListener
+     */
     public void setOnDownloadedListener(OnDownloadedListener onDownListener) {
         this.onDownListener = onDownListener;
     }
 
     private Context context;
 
+    /**
+     * Create the instance
+     *
+     * @param context
+     */
     public AuxiliarPhoto(Context context) {
         this.context = context;
         this.PATH = context.getCacheDir().getAbsolutePath();
         PATH = PATH.replace("cache", "app_");
     }
-    /*
-        private void createDirectoryIfNeeded(String nameDirectory) {
-            File file = new File(getPath(), nameDirectory);
-            if(!file.exists()) {
-                file.mkdirs();
-            }
-        }
-    */
+
+    /**
+     * get the path of internal storage
+     *
+     * @return path
+     */
     public String getPath() {
         return this.PATH;
     }
 
-    /*****************************************************
-     * ********* Baixa a imagem e salva ou apenas seta-a.
+    /**
+     * delete the file if exists
      *
+     * @param dir the dir of file
+     * @param nameImage - the name of file
+     * @return if was deleted
      */
     public boolean deleteIfExists(String dir, String nameImage) {
         boolean retorno = false;
@@ -76,24 +111,37 @@ public class AuxiliarPhoto {
         return retorno;
     }
 
+    /**
+     * return if the file exists
+     *
+     * @param directory
+     * @param nameImage
+     * @return if exists
+     */
     public boolean fileExists(String directory, String nameImage) {
-        boolean retorno = false;
+        boolean ifExists = false;
         String PathLocal = PATH + directory;
         try {
             File f = new File(PathLocal, nameImage);
             if (f.exists()) {
-                retorno = true;
-            } else retorno = false;
+                ifExists = true;
+            } else ifExists = false;
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return retorno;
+        return ifExists;
     }
 
-    /**************************************************************************************
-     *  Baixa a imagem e repassa o bitmap pela Interface
+    /**
+     * save bitmap photo in internal storage
+     *
+     * @param dir
+     * @param bitmapImage
+     * @param nameImage
+     * @param quality - 0
+     * @return
      */
-    public boolean saveToInternalStorage(String dir, Bitmap bitmapImage, String nameImage, int qualidade) {
+    public boolean saveToInternalStorage(String dir, Bitmap bitmapImage, String nameImage, int quality) {
         boolean retorno = false;
         ContextWrapper cw = new ContextWrapper(context);
         // path to /data/data/yourapp/app_data/imageDir
@@ -106,8 +154,8 @@ public class AuxiliarPhoto {
         try {
             fos = new FileOutputStream(mypath);
             // Use the compress method on the BitMap object to write image to the OutputStream
-            if (qualidade < 1) qualidade = 100;
-            bitmapImage.compress(Bitmap.CompressFormat.JPEG, qualidade, fos);
+            if (quality < 1) quality = 100;
+            bitmapImage.compress(Bitmap.CompressFormat.JPEG, quality, fos);
             retorno = true;
         } catch (Exception e) {
             e.printStackTrace();
@@ -123,10 +171,22 @@ public class AuxiliarPhoto {
         return retorno;
     }
 
+    /**
+     * get the context
+     *
+     * @return context
+     */
     public Context getContext() {
         return context;
     }
 
+    /**
+     * try load the imagem from storage based params
+     *
+     * @param dir
+     * @param nameImage
+     * @return bitmap if the photo was found
+     */
     public Bitmap loadImageFromStorage(String dir, String nameImage) {
         Bitmap b = null;
         String PathLocal = PATH + dir;
@@ -144,29 +204,24 @@ public class AuxiliarPhoto {
         return b;
     }
 
-    /*
-    public Bitmap renameFile(String dir, String nameImage, String newName) {
-        Bitmap b = null;
-        String PathLocal = PATH + dir;
-        try {
-            File f = new File(PathLocal, nameImage);
-            File arq = new File(PathLocal, newName);
-            if (f.exists()) {
-                b = BitmapFactory.decodeStream(new FileInputStream(f));
-                f.renameTo(arq);
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
 
-        return b;
-    }
-*/
-
+    /**
+     * create the instance of file based by params
+     *
+     * @param directory
+     * @param nameImage
+     * @return the file instance
+     */
     public File getFile(String directory, String nameImage) {
         return new File(PATH + directory, nameImage);
     }
 
+    /**
+     * delete all files at directory
+     * passed by param
+     *
+     * @param dir
+     */
     public void deletAllFiles(String dir) {
         File f = new File(getPath() + dir);
 
@@ -178,7 +233,11 @@ public class AuxiliarPhoto {
         }
     }
 
-
+    /**
+     * delete the file based by params
+     * @param dir
+     * @param nameImage
+     */
     public void deleteFile(String dir, String nameImage) {
         String PathLocal = PATH + dir;
         try {
@@ -191,11 +250,18 @@ public class AuxiliarPhoto {
         }
     }
 
-    public Bitmap returnBitmapFile(String dir, String nameImage) {
+    /**
+     * get the bitmap based by params
+     *
+     * @param dir
+     * @param namePhoto
+     * @return the bitmap if found
+     */
+    public Bitmap returnBitmapFile(String dir, String namePhoto) {
         Bitmap b = null;
         String PathLocal = PATH + dir;
         try {
-            File f = new File(PathLocal, nameImage);
+            File f = new File(PathLocal, namePhoto);
             if (f.exists()) {
                 b = BitmapFactory.decodeStream(new FileInputStream(f));
             }
@@ -206,7 +272,14 @@ public class AuxiliarPhoto {
         return b;
     }
 
-    public Bitmap loadImageFromStorageOrDownload(String dir, String nameImage, String UrlDownload, OnDownloadedListener onDown) {
+    /**
+     * this function try to found the photo in
+     * internal storage and if not to found it
+     * will to do download
+     *
+     ** @return the photo bitmap
+     */
+    public Bitmap loadImageFromStorageOrDownload(String dir, String nameImage, String urlDownload, OnDownloadedListener onDown) {
 
         if (onDown != null) onDownListener = onDown;
         Bitmap b = null;
@@ -216,7 +289,7 @@ public class AuxiliarPhoto {
             if (f.exists()) {
                 b = BitmapFactory.decodeStream(new FileInputStream(f));
             } else {
-                new DownloadImageTask(UrlDownload, dir, nameImage).execute();
+                new DownloadImageTask(urlDownload, nameImage).execute();
                 return null;
             }
         } catch (Exception e) {
@@ -227,9 +300,16 @@ public class AuxiliarPhoto {
     }
 
 
+    /**
+     * modify the size of bitmap based by params
+     *
+     * @param bmpOriginal
+     * @param newWidth
+     * @return the new bitmap
+     */
     public Bitmap modifySizeOfBitmap(
             /* O nome do método já diz tudo */     Bitmap bmpOriginal,
-            int newWidth, int newHeight) {
+                                                   int newWidth, int newHeight) {
 
 
         if (bmpOriginal.getWidth() <= newWidth) return bmpOriginal;
@@ -259,10 +339,16 @@ public class AuxiliarPhoto {
         return novoBmp;
     }
 
-
+    /**
+     * modify the size of bitmap based by params
+     *
+     * @param bmpOriginal
+     * @param newWidth
+     * @return the new bitmap
+     */
     public Bitmap modifySizeOfBitmap(
             /* O nome do método já diz tudo */     Bitmap bmpOriginal,
-            int newWidth) {
+                                                   int newWidth) {
 
         if (bmpOriginal.getWidth() <= newWidth) return bmpOriginal;
 
@@ -275,7 +361,7 @@ public class AuxiliarPhoto {
         int w = bmpOriginal.getWidth();
         int h = bmpOriginal.getHeight();
 
-        float densityFactor = getContext().getResources().getDisplayMetrics().density;
+        //float densityFactor = getContext().getResources().getDisplayMetrics().density;
         float novoW = newWidth;
         float novoH = newHeight;
 
@@ -290,68 +376,61 @@ public class AuxiliarPhoto {
 
         novoBmp = Bitmap.createBitmap(bmpOriginal, 0, 0, w, h, matrix, true);
 
-        //img.setRotation;
         return novoBmp;
 
     }
 
-
-    // Decodifica a imagem e escala para a redução do consumo de memória
-    //escala padrão
-    public Bitmap modifyScaleOfFile(File f, int escala) {
+    /**
+     * this method modify the scale of image
+     * for um size better, in other words, a
+     * size that don't use very memory
+     *
+     * @param f
+     * @param scale
+     * @return the new bitmap
+     */
+    public Bitmap modifyScaleOfFile(File f, int scale) {
         try {
-            // Decodifica o tamanho da imagem
+            // found the better size of image
             BitmapFactory.Options o = new BitmapFactory.Options();
             o.inJustDecodeBounds = true;
             BitmapFactory.decodeStream(new FileInputStream(f), null, o);
 
-            // O novo tamanho que queremos
-            final int REQUIRED_SIZE = escala;
+            // the new size that you need
+            final int REQUIRED_SIZE = scale;
 
-            // Achar o valor correto para a escala
-            int scale = 1;
-            while (o.outWidth / scale / 2 >= REQUIRED_SIZE &&
-                    o.outHeight / scale / 2 >= REQUIRED_SIZE) {
-                scale *= 2;
+            // found the better value of scale
+            int scaleSystem = 1;
+            while (o.outWidth / scaleSystem / 2 >= REQUIRED_SIZE &&
+                    o.outHeight / scaleSystem / 2 >= REQUIRED_SIZE) {
+                scaleSystem *= 2;
             }
 
-            // Decodifica com o inSampleSize
+            // make the process
             BitmapFactory.Options o2 = new BitmapFactory.Options();
-            o2.inSampleSize = scale;
+            o2.inSampleSize = scaleSystem;
             return BitmapFactory.decodeStream(new FileInputStream(f), null, o2);
         } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
         return null;
     }
 
-
-    /*
-
-
-    public Bitmap recriarBitmap(String imagePath) {
-
-        try {
-            BitmapFactory.Options options = new BitmapFactory.Options();
-        /*Reduzindo a qualidade da imagem para preservar memoria.
-        * Aqui você pode testar a redução que melhor atende sua necessidade
-
-            options.inSampleSize = 2;
-
-            return BitmapFactory.decodeStream(new FileInputStream(imagePath), null, options);
-        } catch (FileNotFoundException e) {
-
-        }
-        return null;
-
-    }
-        */
-
-    private static Bitmap rotationImage(Bitmap bitmap, int angulo) {
+    /**
+     *
+     * to rotate the image based by angle
+     *
+     * @author Caio
+     * @param bitmap
+     * @param angle
+     * @return
+     */
+    private static Bitmap rotationImage(Bitmap bitmap, int angle) {
         // Abre o bitmap a partir do caminho da foto
 
         // Prepara a operação de rotação com o ângulo escolhido
         Matrix matrix = new Matrix();
-        matrix.postRotate(angulo);
+        matrix.postRotate(angle);
 
         // Cria um novo bitmap a partir do original já com a rotação aplicada
         return Bitmap.createBitmap(bitmap, 0, 0,
@@ -359,20 +438,57 @@ public class AuxiliarPhoto {
                 matrix, true);
     }
 
+    /**
+     * the sample verification if the phone is connected
+     * at internet. You need permission "state network" for
+     * it work
+     *
+     * @author Caio
+     *
+     * @param context
+     * @return if is connected
+     */
+    public static boolean isConnected(Context context) {
+        boolean connected = false;
+        ConnectivityManager conectivtyManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (conectivtyManager.getActiveNetworkInfo() != null
+                && conectivtyManager.getActiveNetworkInfo().isAvailable()
+                && conectivtyManager.getActiveNetworkInfo().isConnected()) {
+            connected = true;
+        } else {
+            connected = false;
+        }
+        return connected;
+    }
 
+
+    /**
+     * this class make the download of photo based by path
+     *
+     */
     private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
 
         private String pathDown;
         private String nameImage;
-        private String dir;
 
-        public DownloadImageTask(String PathDownload, String dir, String nameImage) {
+        /**
+         * constructor with the params nedded
+         *
+         * @param PathDownload - the url
+         * @param nameImage
+         */
+        public DownloadImageTask(String PathDownload, String nameImage) {
             pathDown = PathDownload;
             this.nameImage = nameImage;
-            this.dir = dir;
 
         }
 
+        /**
+         * make the download in word thead
+         * @param params
+         * @return the photo or null if not connected
+         */
+        @WorkerThread
         @Override
         protected Bitmap doInBackground(String... params) {
             Bitmap bitmap = null;
@@ -382,38 +498,26 @@ public class AuxiliarPhoto {
             } catch (Exception e) {
                 e.printStackTrace();
             }
+
             if (onDownListener != null)
                 bitmap = onDownListener.onDownloadCompletedInBackGround(bitmap, nameImage);
+
             return bitmap;
         }
 
+        /**
+         * happen after execute in doinBackground
+         * here's in main thread.
+         *
+         * @param bitmap
+         */
         @Override
-        protected void onPostExecute(Bitmap bitmap) {
+        protected void onPostExecute(@Nullable Bitmap bitmap) {
             if (onDownListener != null) {
                 onDownListener.onDownloadCompleted(bitmap, nameImage);
-                bitmap = null;
             }
         }
 
     }
-
-
-    public static boolean isConnected(Context context) {
-        boolean conectado = false;
-        try {
-            ConnectivityManager conectivtyManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-            if (conectivtyManager.getActiveNetworkInfo() != null
-                    && conectivtyManager.getActiveNetworkInfo().isAvailable()
-                    && conectivtyManager.getActiveNetworkInfo().isConnected()) {
-                conectado = true;
-            } else {
-                conectado = false;
-            }
-        } catch (Exception e) {
-            return false;
-        }
-        return conectado;
-    }
-
 
 }
--- auxphoto/src/main/java/caiohenrique/auxphoto/DownPhotos.java
+++ auxphoto/src/main/java/caiohenrique/auxphoto/DownPhotos.java
@@ -3,136 +3,251 @@ package caiohenrique.auxphoto;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
-import android.view.View;
-import android.widget.ImageView;
+import android.support.annotation.WorkerThread;
 
 import java.io.InputStream;
 import java.net.URL;
 import java.util.List;
 
 /**
+ *
+ * make the download of a lot of images
+ * one by one
+ *
  * Created by Caio on 20/10/2017.
  */
-
 public class DownPhotos {
-    private boolean atividade = true;
-    List<String> mList;
-    OnDownloadedCompleted inter;
-    public String UrlBaseEvents;
-    private int ListaTamanho = 0;
-
-    private int timeEnterDownImages = 215;
-    /********************************************
-     * Quando true aguarda continuação do serviço
+    /**
+     * if is download the photos yet
      */
-    private boolean VERIFICAPAUSA = false;
+    private boolean activity = true;
+
+    /**
+     * save the list of urls
+     */
+    List<String> mList;
+
+    /**
+     * listener of download
+     */
+    OnDownloadedCompleted inter;
+
+    /**
+     * the time between downloads
+     */
+
+    private int timeBetweenDownImages = 215;
+
+    /**
+     * when true the system stop
+     * the download
+     * but not stop de process
+     *
+     * if you need stop de process
+     * turn activity = false
+     *
+     */
+    private boolean verifyPause = false;
+
+    /**
+     * the listener
+     */
     private DownloadImageTaskImage downImagesTask;
 
+    /**
+     * initialize the class and start the downloads
+     *
+     * @param mlist
+     * @param inter
+     */
     public DownPhotos(List<String> mlist, OnDownloadedCompleted inter) {
         this.mList = mlist;
-        this.ListaTamanho = mlist.size();
         this.inter = inter;
-        comecaDown();
+        startDown();
     }
 
+    /**
+     * return if is stopped
+     *
+     * @return if stopped
+     */
     public boolean isStopped() {
-        return VERIFICAPAUSA;
+        return verifyPause;
     }
 
+    /**
+     * stop de service
+     */
     public void stop() {
-        VERIFICAPAUSA = true;
+        verifyPause = true;
     }
 
+    /**
+     * return the download when is stop
+     */
     public void reestart() {
-        VERIFICAPAUSA = false;
+        verifyPause = false;
     }
 
-    public boolean verificaAtividade() {
-        return atividade;
+    /**
+     * return if is active
+     *
+     * @return if active
+     */
+    public boolean isActive() {
+        return activity;
     }
 
+    /**
+     * finish the activity of downloads
+     */
     public void finishDownloads() {
-        atividade = false;
+        activity = false;
     }
 
-    public void setTimeEnterDownImages(int newTime) {
-        timeEnterDownImages = newTime;
+    /**
+     * change the time between downloads
+     *
+     * @param newTime
+     */
+    public void setTimebeteweenDownImages(int newTime) {
+        timeBetweenDownImages = newTime;
     }
 
-    private void comecaDown() {
+    /**
+     * start the download
+     */
+    private void startDown() {
 
-        downImagesTask = new DownloadImageTaskImage(mList.get(contagem), inter);
+        downImagesTask = new DownloadImageTaskImage(mList.get(count), inter);
         downImagesTask.execute();
     }
 
-    int contagem = 0;
+    /**
+     * save what number of list stopped
+     */
+    int count = 0;
 
+    /**
+     * notify when need make more downloads
+     *
+     * @param moreDownloads
+     */
     public void notifyAddMoreImages(List<String> moreDownloads) {
-        if (atividade) {
+        if (activity) {
             for (String pt : moreDownloads) {
                 mList.add(pt);
             }
-            ListaTamanho = mList.size();
 
         } else {
-            atividade = true;
+            activity = true;
             mList = moreDownloads;
-            ListaTamanho = mList.size();
-            contagem = 0;
-            comecaDown();
+            count = 0;
+            startDown();
         }
     }
 
+    /**
+     * the listener of download
+     */
     public interface OnDownloadedCompleted {
-
+        /**
+         * called when the download was completed
+         *
+         * @param path - path of download
+         * @param bitmap - bitmap
+         */
         void onDownloadedCompleted(String path, Bitmap bitmap);
 
-        void onDownloadedCompletedInBackground(String path, Bitmap bitmap);
+        /**
+         * called when the download is completed yet in work thread
+         * the user can return the bitmap modified
+         * @param path
+         * @param bitmap
+         */
+        @WorkerThread
+        Bitmap onDownloadedCompletedInBackground(String path, Bitmap bitmap);
 
     }
 
+    /**
+     * called always that complet one cycle of donwload
+     * @param path
+     * @param bitmap
+     */
     private void onDownloadedCompleted(String path, Bitmap bitmap) {
-        if (atividade) {
+        if (activity) {
             /*
-            * Null faremos de novo, se não vamos para o próximo
+             * Null faremos de novo, se não vamos para o próximo
              */
             if (bitmap != null) {
-                contagem++;
+                count++;
                 if (inter != null) {
                     inter.onDownloadedCompleted(path, bitmap);
                 }
-                if (contagem < ListaTamanho) {
-                    comecaDown();
+                if (count < getSizeOfList() ) {
+                    startDown();
                 } else {
-                    atividade = false;
+                    activity = false;
                 }
             } else {
-                comecaDown();
+                startDown();
             }
         }
     }
 
+    /**
+     * get the size of list
+     *
+     * @return size
+     */
+    public int getSizeOfList() {
+        return mList.size();
+    }
+
+
+    /**
+     * the class that to do photos download
+     */
     private class DownloadImageTaskImage extends AsyncTask<String, Void, Bitmap> {
 
+        /**
+         * the listener
+         */
         private OnDownloadedCompleted inter;
+        /*
+        path of download
+         */
         private String path;
 
+        /**
+         * initialize the class
+         *
+         * @param path
+         * @param inter
+         */
         public DownloadImageTaskImage(String path, OnDownloadedCompleted inter) {
             this.inter = inter;
             this.path = path;
         }
 
+        /**
+         * make the download of bitmap
+         *
+         * @param params
+         * @return
+         */
         @Override
         protected Bitmap doInBackground(String... params) {
             Bitmap bitmap = null;
 
-            while (VERIFICAPAUSA) {
+            while (verifyPause) {
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                if (!atividade) break;
+                if (!activity) break;
             }
             try {
                 InputStream in = new URL(path).openStream();
@@ -141,21 +256,24 @@ public class DownPhotos {
                 e.printStackTrace();
             }
             try {
-                Thread.sleep(timeEnterDownImages);
+                Thread.sleep(timeBetweenDownImages);
             } catch (Exception e) { }
 
             if (bitmap != null && inter != null)
-                inter.onDownloadedCompletedInBackground(path, bitmap);
+                return inter.onDownloadedCompletedInBackground(path, bitmap);
 
             return bitmap;
         }
 
+        /**
+         * after try make the download
+         * @param bitmap
+         */
         @Override
         protected void onPostExecute(Bitmap bitmap) {
             DownPhotos.this.onDownloadedCompleted(path, bitmap);
             bitmap = null;
         }
     }
-
 
 }
